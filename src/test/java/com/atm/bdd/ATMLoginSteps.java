45fe28e1605fbc02a7553799c7def12278c48c7d
CGM <mrmouse.guru@gmail.com>
2026-02-03 08:14:28 +0700

    initialize ATM

---
+++ src/test/java/com/atm/bdd/ATMLoginSteps.java
@@ -0,0 +1,22 @@
+package com.atm.bdd;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ATMLoginSteps {
+
+    @Given("Thẻ ATM hợp lệ")
+    public void validCard(){
+
+    }
+    @When("chủ thẻ nhập đúng mã PIN")
+    public void enter_pin(){
+
+    }
+    @Then("ATM cho phép truy cập")
+    public void atmAllowsAccess(){
+
+    }
+
+}
---
+++ src/test/resources/com/atm/atm_login.feature
@@ -0,0 +1,6 @@
+Feature: Login
+AC1 (Happy Path): Đăng nhập thành công
+Given: Thẻ ATM hợp lệ
+When: chủ thẻ nhập đúng mã PIN
+Then: ATM cho phép truy cập
+
